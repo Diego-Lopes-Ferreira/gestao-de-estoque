c7c48143ac2fce5b7ab0404f41a1342251d2ff18
Diego-Lopes-Ferreira <dglpferreira@gmail.com>
2023-06-03 21:40:49 -0300

    feat(styles): Main screen and nav
      + Added title and text to main screen
      + Added a sidebar menu to main screen

--- pages/index.jsx
+++ pages/index.jsx
@@ -24,21 +24,30 @@ export default function HomePage() {
 
   return (
     <main>
-      <h1>Relatório de Produtos</h1>
-      <Link href="/users" >Gerenciar Pessoas</Link>
-      <Link href="/products" >Gerenciar Produtos</Link>
-      <Link href="/transactions" >Gerenciar Transacoes</Link>
-      <Link href="/alerts" >Gerenciar Alertas</Link>
-      <Link href="/tasks" >Gerenciar Tarefas</Link>
-      {/* TODO: Tarefa 4.05 */ }
+      <nav>
+        <h1>Menu</h1>
+        <h2>Gerenciamento de Cadastro</h2>
+        <Link href="/users">Usuários</Link>
+        <Link href="/products">Produtos</Link>
+        <Link href="/alerts">Alertas*</Link>
+        <h2>Ações</h2>
+        <Link href="/transactions" >Transações</Link>
+        <Link href="/tasks" >Tarefas</Link>
+      </nav>
       <section>
+        <h1>Relatório de Produtos</h1>
+        <p className="explanation">
+          Aqui são mostrados quais os produtos que estão cadastrados no estoque
+          e qual a quantidade de cada um. Clique em um produto para editar seu
+          registro.
+        </p>
         {
           products.map(product => {
-            return <Link key={product.id} href={`/products/${product.id}`}>
+            return <Link key={ product.id } href={ `/products/${product.id}` }>
               <div className="item">
-                <p>{product.name}: <strong>{product.quantity}</strong></p>
+                <p>{ product.name }: <strong>{ product.quantity }</strong></p>
               </div>
-            </Link>
+            </Link>;
           })
         }
       </section>
